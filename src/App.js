05e499694f3b555738c451f2db77a2f0ba66e74d
jjs951213 <jjs951213@naver.com>
2020-09-12 17:33:51 +0900

    13.4.1 URL파라미터

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 
 const App = () => {
@@ -14,10 +15,17 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };
