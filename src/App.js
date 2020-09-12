31856424f883a019bf896dc256bc52c6ac545e00
jjs951213 <jjs951213@naver.com>
2020-09-12 16:58:29 +0900

    13.2.4-1 Route 컴포넌트로 특정 주소레 컴포넌트 연결

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+ 
+const App = () => {
+  return (
+    <div>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  );
+};
